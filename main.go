e53c7fc5a1d0bbb99dbb9dbbb460fbf0774d6c6d
Raymond Rafael Chen <57492535+AgapeKagemine@users.noreply.github.com>
2024-07-24 11:15:03 +0700

    fix: Better race condition handling
    refactor code to use lock

--- main.go
+++ main.go
@@ -11,46 +11,41 @@ type Account struct {
 	mut     *sync.Mutex
 }
 
-var now = time.Now()
-
-func addBalance(account *Account, wg *sync.WaitGroup) {
+func addBalance(account *Account, wg *sync.WaitGroup, now time.Time) {
 	defer wg.Done()
-
-	if account.mut.TryLock() {
-		account.balance += 100
-		fmt.Println("ADD    : \t", account.balance, "\t", time.Since(now).Microseconds(), "ms")
-		account.mut.Unlock()
-	} else {
-		fmt.Println("ADD    :\t IS \t WAITING")
-	}
+	account.mut.Lock()
+	account.balance += 100
+	fmt.Println("ADD     : \t", account.balance, "\t", time.Since(now).Microseconds(), "ms")
+	account.mut.Unlock()
 }
 
-func deductBalance(account *Account, wg *sync.WaitGroup) {
+func deductBalance(account *Account, wg *sync.WaitGroup, now time.Time) {
 	defer wg.Done()
-
-	if account.mut.TryLock() {
-		account.balance -= 100
-		fmt.Println("DEDUCT : \t", account.balance, "\t", time.Since(now).Microseconds(), "ms")
-		account.mut.Unlock()
-	} else {
-		fmt.Println("DEDUCT :\t IS \t WAITING")
-	}
+	account.mut.Lock()
+	account.balance -= 100
+	fmt.Println("DEDUCT  : \t", account.balance, "\t", time.Since(now).Microseconds(), "ms")
+	account.mut.Unlock()
 }
 
 func main() {
-	var account = Account{
+	now := time.Now()
+
+	account := Account{
 		balance: 1000,
 		mut:     &sync.Mutex{},
 	}
 
+	fmt.Println("INITIAL : \t", account.balance, "\t", time.Since(now).Microseconds(), "ms")
+
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100; i++ {
 		wg.Add(2)
-		go addBalance(&account, &wg)
-		go deductBalance(&account, &wg)
+		go addBalance(&account, &wg, now)
+		go deductBalance(&account, &wg, now)
 	}
 
 	wg.Wait()
 
+	fmt.Println("RESULT : \t", account.balance, "\t", time.Since(now).Microseconds(), "ms")
 }
